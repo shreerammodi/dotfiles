688ef8141782c2e0d2133abab6019cc2a7c1a7b1
Shreeram Modi <smodi@smodi.net>
2024-08-10 16:17:43 -0700

    Add all dotfiles

---
+++ nvim/.config/nvim/lua/shreeram/options.lua
@@ -0,0 +1,31 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.undofile = true
+vim.opt.updatetime = 500
+vim.opt.timeoutlen = 500
+
+vim.opt.wrap = false
+
+vim.opt.ruler = true
+vim.opt.scrolloff = 3
+vim.opt.showmode = false
+vim.opt.title = true
+
+vim.opt.modeline = false
+
+vim.opt.termguicolors = true
+
+vim.g.neovide_window_blurred = true
+vim.o.guifont = "IBM Plex Mono:h12"
