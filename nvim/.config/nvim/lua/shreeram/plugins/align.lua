e69343faf8daeff214f921cdaca422f450315deb
Shreeram Modi <smodi@smodi.net>
2024-10-08 15:59:09 -0400

    nvim: Fix align lazy loading
    For lazy loading on keypress, if modes are not specified it defaults to
    only normal mode. Since these keybindings are used in visual mode as
    well, visual mode needs to be specified to load the plugin.

--- nvim/.config/nvim/lua/shreeram/plugins/align.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/align.lua
@@ -1,6 +1,9 @@
 return {
-	"echasnovski/mini.align",
-	commit = "26111a737494bc39576ad67b52bbc2638ca23553",
-	keys = { "ga", "gA" },
-	config = true,
+    "echasnovski/mini.align",
+    commit = "26111a737494bc39576ad67b52bbc2638ca23553",
+    keys = {
+        { "ga", mode = { "n", "v" } },
+        { "gA", mode = { "n", "v" } }
+    },
+    config = true,
 }
