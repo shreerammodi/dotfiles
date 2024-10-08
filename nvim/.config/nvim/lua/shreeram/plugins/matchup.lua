3038a862f0cbe96e56baa664acfc04e4e0648549
Shreeram Modi <smodi@smodi.net>
2024-10-08 15:43:42 -0400

    nvim: Fix lazy loading of matchup
    For some reason, loading matchup only on particular keys doesn't work,
    it needs to be loaded on startup.

--- nvim/.config/nvim/lua/shreeram/plugins/matchup.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/matchup.lua
@@ -1,11 +1,5 @@
 return {
-	"andymass/vim-matchup",
-	commit = "1535a769d5dca851fe7d41b0be95f7c7203a4bef",
-	keys = {
-		"%",
-		"[%",
-		"]%",
-		"g%",
-		"z%",
-	},
+    "andymass/vim-matchup",
+    commit = "1535a769d5dca851fe7d41b0be95f7c7203a4bef",
+    event = "VeryLazy"
 }
