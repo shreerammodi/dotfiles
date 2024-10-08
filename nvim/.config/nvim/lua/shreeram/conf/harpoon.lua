b68eaf14e952d55579feae56833ef2013d7d23a2
Shreeram Modi <smodi@smodi.net>
2024-10-08 15:42:32 -0400

    nvim: Update harpoon keybindings
    Add descriptions for which-key

--- nvim/.config/nvim/lua/shreeram/conf/harpoon.lua
+++ nvim/.config/nvim/lua/shreeram/conf/harpoon.lua
@@ -2,35 +2,74 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>bm", function()
-	harpoon:list():add()
-end)
+vim.keymap.set(
+    "n",
+    "<leader>bm", function()
+        harpoon:list():add()
+    end,
+    { desc = "Add to harpoon" }
+)
 
-vim.keymap.set("n", "<leader><return>", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
+vim.keymap.set(
+    "n",
+    "<leader><return>",
+    function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+    end,
+    { desc = "Harpoon list" }
+)
 
-vim.keymap.set("n", "<leader>1", function()
-	harpoon:list():select(1)
-end)
+vim.keymap.set(
+    "n",
+    "<leader>1",
+    function()
+        harpoon:list():select(1)
+    end,
+    { desc = "Harpoon goto 1" }
+)
 
-vim.keymap.set("n", "<leader>2", function()
-	harpoon:list():select(2)
-end)
+vim.keymap.set(
+    "n",
+    "<leader>2",
+    function()
+        harpoon:list():select(2)
+    end,
+    { desc = "Harpoon goto 2" }
+)
 
-vim.keymap.set("n", "<leader>3", function()
-	harpoon:list():select(3)
-end)
+vim.keymap.set(
+    "n",
+    "<leader>3",
+    function()
+        harpoon:list():select(3)
+    end,
+    { desc = "Harpoon goto 3" }
+)
 
-vim.keymap.set("n", "<leader>4", function()
-	harpoon:list():select(4)
-end)
+vim.keymap.set(
+    "n",
+    "<leader>4",
+    function()
+        harpoon:list():select(4)
+    end,
+    { desc = "Harpoon goto 4" }
+)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<leader>bp", function()
-	harpoon:list():prev()
-end)
+vim.keymap.set(
+    "n",
+    "<leader>bp",
+    function()
+        harpoon:list():prev()
+    end,
+    { desc = "Harpoon previous" }
+)
 
-vim.keymap.set("n", "<leader>bn", function()
-	harpoon:list():next()
-end)
+vim.keymap.set(
+    "n",
+    "<leader>bn",
+    function()
+        harpoon:list():next()
+    end,
+    { desc = "Harpoon next" }
+)
