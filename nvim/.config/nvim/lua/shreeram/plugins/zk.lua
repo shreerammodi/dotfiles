593379ecb81bc50651e6a75e66fe63ccf46cc622
Shreeram Modi <smodi@smodi.net>
2025-04-26 08:57:19 -0400

    nvim: add zk plugin

---
+++ nvim/.config/nvim/lua/shreeram/plugins/zk.lua
@@ -0,0 +1,11 @@
+return {
+    "zk-org/zk-nvim",
+    config = function()
+        require("zk").setup({
+            picker = "telescope",
+            picker_options = {
+                telescope = require("telescope.themes").get_ivy(),
+            }
+        })
+    end
+}
