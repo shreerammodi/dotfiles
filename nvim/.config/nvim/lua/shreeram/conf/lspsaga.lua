9728001decfc792cd4d27c47fc4e55b2de3ef96d
Shreeram Modi <smodi@smodi.net>
2025-04-26 08:57:50 -0400

    nvim: add lspsaga keybindings

--- nvim/.config/nvim/lua/shreeram/conf/lspsaga.lua
+++ nvim/.config/nvim/lua/shreeram/conf/lspsaga.lua
@@ -1,7 +1,26 @@
 require("lspsaga").setup({
-	lightbulb = {
-		enable = false,
-	},
+    lightbulb = {
+        enable = false,
+    },
 })
 
-vim.keymap.set("n", "<leader>lr", "<CMD>Lspsaga rename<CR>", { desc = "Switch buffer" })
+vim.diagnostic.config({
+    severity_sort = true,
+})
+
+vim.keymap.set("n", "<leader>lr", "<CMD>Lspsaga rename<CR>", { desc = "Rename" })
+
+vim.keymap.set("n", "<leader>ll", "<CMD>Lspsaga show_line_diagnostics<CR>", { desc = "Line Diagnostics" })
+vim.keymap.set("n", "<leader>lc", "<CMD>Lspsaga show_cursor_diagnostics<CR>", { desc = "Cursor Diagnostics" })
+vim.keymap.set("n", "<leader>lb", "<CMD>Lspsaga show_buf_diagnostics<CR>", { desc = "Buffer Diagnostics" })
+
+vim.keymap.set("n", "<leader>lo", "<CMD>Lspsaga outline<CR>", { desc = "Outline" })
+vim.keymap.set("n", "<leader>la", "<CMD>Lspsaga code_action<CR>", { desc = "Code Action" })
+vim.keymap.set("n", "<leader>lh", "<CMD>Lspsaga hover_doc<CR>", { desc = "Hover Doc" })
+vim.keymap.set("n", "<leader>lf", "<CMD>Lspsaga finder<CR>", { desc = "Finder" })
+
+vim.keymap.set("n", "<leader>li", "<CMD>Lspsaga diagnostic_jump_next<CR>", { desc = "Next Diagnostic" })
+vim.keymap.set("n", "<leader>lo", "<CMD>Lspsaga diagnostic_jump_prev<CR>", { desc = "Prev Diagnostic" })
+
+vim.keymap.set("n", "<leader>ldg", "<CMD>Lspsaga goto_definition<CR>", { desc = "Goto Definition" })
+vim.keymap.set("n", "<leader>ldp", "<CMD>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
