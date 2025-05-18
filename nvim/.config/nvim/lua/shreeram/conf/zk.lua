1042f2ea5f5cbb7d2419753c8d3a3d7e62355cbf
Shreeram Modi <smodi@smodi.net>
2025-05-18 00:24:25 -0700

    nvim: add zk keybinds

--- nvim/.config/nvim/lua/shreeram/conf/zk.lua
+++ nvim/.config/nvim/lua/shreeram/conf/zk.lua
@@ -2,11 +2,38 @@ local zk = require("zk.commands")
 
 vim.keymap.set(
     "n",
+    "<leader>nb",
+    function()
+        zk.get("ZkBacklinks")()
+    end,
+    { desc = "Browse backlinks" }
+)
+
+vim.keymap.set(
+    "n",
     "<leader>nf",
     function()
-        zk.get("ZkNotes")
+        zk.get("ZkNotes")()
     end,
     { desc = "Browse notes" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>ni",
+    function()
+        zk.get("ZkInsertLink")()
+    end,
+    { desc = "Insert link" }
+)
+
+vim.keymap.set(
+    "x",
+    "<leader>ni",
+    function()
+        zk.get("ZkInsertLink")()
+    end,
+    { desc = "Insert link at selection" }
 )
 
 vim.keymap.set(
