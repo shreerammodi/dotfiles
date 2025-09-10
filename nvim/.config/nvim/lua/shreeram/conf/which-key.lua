ae1ef2714a6424fdcdd8d4f0a09a01bd883109c3
Shreeram Modi <smodi@smodi.net>
2025-09-10 14:06:13 -0400

    nvim: autoformat

--- nvim/.config/nvim/lua/shreeram/conf/which-key.lua
+++ nvim/.config/nvim/lua/shreeram/conf/which-key.lua
@@ -1,9 +1,11 @@
 local wk = require("which-key")
 
 wk.add({
-	{ "<leader>b", group = "buffer" },
-	{ "<leader>f", group = "file" },
-	{ "<leader>g", group = "git" },
-	{ "<leader>o", group = "obsidian" },
-	{ "<leader>s", group = "search" },
+    { "<leader>b", group = "buffer" },
+    { "<leader>f", group = "file" },
+    { "<leader>g", group = "git" },
+    { "<leader>l", group = "Trouble" },
+    { "<leader>n", group = "notes" },
+    { "<leader>o", group = "obsidian" },
+    { "<leader>s", group = "search" },
 })
