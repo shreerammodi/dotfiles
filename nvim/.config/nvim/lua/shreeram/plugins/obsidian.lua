cf8c498008a76015f28b13846e7657632ac1c326
Shreeram Modi <smodi@smodi.net>
2025-09-10 14:05:56 -0400

    nvim: add keybindings for obsidian

--- nvim/.config/nvim/lua/shreeram/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/obsidian.lua
@@ -4,13 +4,16 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    opts = {
-        disable_frontmatter = true,
-        workspaces = {
-            {
-                name = "notes",
-                path = "~/Documents/notes",
-            },
-        }
-    }
+    cmd = "Obsidian",
+    keys = {
+        { "<leader>nd", desc = "Daily notes" },
+        { "<leader>nD", desc = "Today's daily" },
+        { "<leader>nt", desc = "Search tags" },
+        { "<leader>ns", desc = "Search vault" },
+        { "<leader>nf", desc = "Find note" },
+
+    },
+    config = function()
+        require("shreeram.conf.obsidian")
+    end,
 }
