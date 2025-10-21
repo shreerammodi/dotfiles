958e61ba92737c18439ab1f7ab82ce64edc239d3
Shreeram Modi <smodi@smodi.net>
2025-10-21 13:04:22 -0400

    nvim: format

--- nvim/.config/nvim/lua/shreeram/plugins/neogit.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/neogit.lua
@@ -2,9 +2,8 @@ return {
     "NeogitOrg/neogit",
     commit = "63124cf520ff24d09deb3b850e053908ab0fc66a",
     dependencies = {
-        { "nvim-lua/plenary.nvim",         lazy = true },
-        { "sindrets/diffview.nvim",        lazy = true },
-        { "nvim-telescope/telescope.nvim", lazy = true },
+        { "nvim-lua/plenary.nvim",  lazy = true },
+        { "sindrets/diffview.nvim", lazy = true },
     },
     config = function()
         require("shreeram.conf.neogit")
