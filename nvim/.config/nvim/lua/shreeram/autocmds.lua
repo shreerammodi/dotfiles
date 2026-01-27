5c32308cec7e7427febe6f5da9747e72f2448a99
Shreeram Modi <smodi@smodi.net>
2026-01-27 16:13:58 -0500

    nvim: add js filetypes
    GoogleScript files should be recnogized as javascript

--- nvim/.config/nvim/lua/shreeram/autocmds.lua
+++ nvim/.config/nvim/lua/shreeram/autocmds.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.gs",
+  callback = function()
+    vim.bo.filetype = "javascript"
+  end,
+})
