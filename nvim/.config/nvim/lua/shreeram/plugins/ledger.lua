7e615380f4bfedd307eeab9554fd714a0c15e630
Shreeram Modi <smodi@smodi.net>
2026-02-21 19:43:29 -0500

    nvim: replace vim-ledger with ledger.nvim

--- nvim/.config/nvim/lua/shreeram/plugins/ledger.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/ledger.lua
@@ -1,5 +1,43 @@
 return {
-    'ledger/vim-ledger',
-    ft = 'ledger',
-    commit = '4f2d93dd914f625e2d3db47d97bd9f428fedd68f'
+    'wllfaria/ledger.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    ft = { 'ledger' },
+    config = function()
+        require('ledger').setup({
+            extensions = { 'ledger', 'hledger', 'journal' },
+            completion = {
+                cmp = { enabled = true },
+            },
+            diagnostics = {
+                lsp_diagnostics = true,
+                strict = false,
+            },
+            snippets = {
+                native = { enabled = false },
+                cmp    = { enabled = false },
+                luasnip = { enabled = true },
+            },
+            keymaps = {
+                snippets = {
+                    new_posting       = { 'tt' },
+                    new_account       = { 'acc' },
+                    new_posting_today = { 'td' },
+                    new_commodity     = { 'cm' },
+                },
+                reports = {
+                    { key = '<leader>hb', name = 'Balance',  command = 'hledger bal' },
+                    { key = '<leader>hr', name = 'Register', command = 'hledger reg' },
+                    { key = '<leader>hc', name = 'Check',    command = 'hledger check' },
+                },
+                tui = {
+                    initialize = { '<leader>ht' },
+                    shutdown   = { '<leader>hq' },
+                },
+            },
+            tui = {
+                enabled     = true,
+                open_in_tab = true,
+            },
+        })
+    end,
 }
