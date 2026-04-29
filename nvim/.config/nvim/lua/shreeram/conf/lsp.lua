4cd3e18fe7c304a4eb09de9705ebfa2e7628ed8a
Shreeram Modi <smodi@smodi.net>
2026-04-29 16:15:09 -0400

    nvim: Add lsp stop command

--- nvim/.config/nvim/lua/shreeram/conf/lsp.lua
+++ nvim/.config/nvim/lua/shreeram/conf/lsp.lua
@@ -7,6 +7,10 @@ local lsp_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 vim.keymap.set({ "n", "x" }, "<leader>lr", function() vim.lsp.buf.rename() end)
 
+vim.api.nvim_create_user_command("StopLSP", function()
+    vim.lsp.stop_client(vim.lsp.get_clients())
+end, {})
+
 mason.setup({
     ensure_installed = {
         "asmfmt",
