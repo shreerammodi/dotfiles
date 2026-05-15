a2415289930b67738bcf4eb47816e8508e3b86f8
Shreeram Modi <smodi@smodi.net>
2026-05-15 12:13:06 -0400

    nvim: update deprecated method

--- nvim/.config/nvim/lua/shreeram/conf/lsp.lua
+++ nvim/.config/nvim/lua/shreeram/conf/lsp.lua
@@ -141,7 +141,7 @@ null_ls.setup({
         },
     },
     on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
+        if client:supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = lsp_augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = lsp_augroup,
