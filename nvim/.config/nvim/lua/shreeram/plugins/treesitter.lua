2af5288196407333d3fb0cd4b6451c9d1a67918b
Shreeram Modi <smodi@smodi.net>
2026-06-18 01:37:49 -0700

    nvim: update treesitter to main

--- nvim/.config/nvim/lua/shreeram/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    version = "10.0.0",
-    event = "VeryLazy",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
     config = function()
         require("shreeram.conf.treesitter")
-    end,
-    build = ":TSUpdate"
+    end
 }
