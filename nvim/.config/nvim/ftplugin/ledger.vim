1a2500afe44cb9a76f62e5b4b4a4fbe5e3df431e
Shreeram Modi <smodi@smodi.net>
2026-06-18 01:38:00 -0700

    nvim: format

--- nvim/.config/nvim/ftplugin/ledger.vim
+++ nvim/.config/nvim/ftplugin/ledger.vim
@@ -4,6 +4,6 @@ setlocal noexpandtab
 setlocal tabstop=4
 setlocal shiftwidth=4
 
-" Disable autopairs — square/paren brackets are meaningful ledger syntax:
+" Disable autopairs---square/paren brackets are meaningful ledger syntax:
 "   [virtual balanced postings] and (virtual unbalanced postings)
 let b:autopairs_enabled = 0
--- nvim/.config/nvim/lua/shreeram/autocmds.lua
+++ nvim/.config/nvim/lua/shreeram/autocmds.lua
@@ -1,13 +1,14 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.gs",
-  callback = function()
-    vim.bo.filetype = "javascript"
-  end,
+    pattern = "*.gs",
+    callback = function()
+        vim.bo.filetype = "javascript"
+    end,
+})
 })
