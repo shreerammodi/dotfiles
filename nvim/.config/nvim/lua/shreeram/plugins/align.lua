8551459fe889fff941f4b803229a3237be2aeff6
Shreeram Modi <smodi@smodi.net>
2026-06-19 16:00:19 -0700

    nvim: update align

--- nvim/.config/nvim/lua/shreeram/plugins/align.lua
+++ nvim/.config/nvim/lua/shreeram/plugins/align.lua
@@ -1,6 +1,5 @@
 return {
-    "echasnovski/mini.align",
-    commit = "3bdf6f0b91b31db5300a7b04f53f296a7fb150c1",
+    "nvim-mini/mini.align",
     keys = {
         { "ga", mode = { "n", "v" } },
         { "gA", mode = { "n", "v" } },
