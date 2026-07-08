a40f98b2b610c6e6503b870f37aa8998e2072756
Shreeram Modi <smodi@smodi.net>
2026-07-07 18:18:18 -0700

    nvim: use catppuccin theme

--- nvim/.config/nvim/lua/smodi/plugins/ui.lua
+++ nvim/.config/nvim/lua/smodi/plugins/ui.lua
@@ -1,24 +1,11 @@
 add({
-    gh("projekt0n/github-nvim-theme"),
+    { src = gh("catppuccin/nvim"), name = "catppuccin" }
 })
 
-require("github-theme").setup({
-    options = {
-        transparent = true,
-        styles = {
-            comments = "italic",
-        },
-        inverse = {
-            visual = false,
-        },
-        modules = {
-            cmp = { enable = true },
-            dapui = { enable = true },
-            gitsigns = { enable = true },
-            native_lsp = { enable = true },
-            neogit = { enable = true },
-        },
-    },
+require("catppuccin").setup({
+    integrations = {
+        neogit = true,
+    }
 })
 
-vim.cmd.colorscheme("github_dark_default")
+vim.cmd.colorscheme("catppuccin-nvim")
