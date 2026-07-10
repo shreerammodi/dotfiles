b75e57fb9847efca2a2a682ce3d82fb923be911c
Shreeram Modi <smodi@smodi.net>
2026-07-10 02:08:53 -0700

    nvim: format

--- nvim/.config/nvim/lua/smodi/plugins/tools.lua
+++ nvim/.config/nvim/lua/smodi/plugins/tools.lua
@@ -8,19 +8,35 @@ add({
 
 local builtin = require("telescope.builtin")
 
-map("n", "<leader>.", function() builtin.find_files() end, { desc = "Find File" })
-map("n", "<leader>ff", function() builtin.find_files() end, { desc = "Find File" })
+map("n", "<leader>.", function()
+    builtin.find_files()
+end, { desc = "Find File" })
+map("n", "<leader>ff", function()
+    builtin.find_files()
+end, { desc = "Find File" })
 
-map("n", "<leader><leader>", function() builtin.git_files() end, { desc = "Find Project File" })
-map("n", "<leader>pf", function() builtin.git_files() end, { desc = "Find Project File" })
+map("n", "<leader><leader>", function()
+    builtin.git_files()
+end, { desc = "Find Project File" })
+map("n", "<leader>pf", function()
+    builtin.git_files()
+end, { desc = "Find Project File" })
 
-map("n", "<leader>/", function() builtin.live_grep() end, { desc = "Grep" })
+map("n", "<leader>/", function()
+    builtin.live_grep()
+end, { desc = "Grep" })
 
-map({"n","x"}, "<leader>sw", function() builtin.grep_string() end, {desc = "Grep word"})
+map({ "n", "x" }, "<leader>sw", function()
+    builtin.grep_string()
+end, { desc = "Grep word" })
 
-map("n", '<leader>s"', function() builtin.registers() end, {desc = "Registers"})
+map("n", '<leader>s"', function()
+    builtin.registers()
+end, { desc = "Registers" })
 
-map("n", "<leader>,", function() builtin.buffers() end, { desc = "Buffers" })
+map("n", "<leader>,", function()
+    builtin.buffers()
+end, { desc = "Buffers" })
 
 require("oil").setup({
     default_file_explorer = true,
